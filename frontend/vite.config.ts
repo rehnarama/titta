be06935b787350f673fd1fc76be77a645fd07056
Astrid Rehn <1352555+rehnarama@users.noreply.github.com>
2026-05-03 15:18:36 +0200

    chore: Add wrangler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import { assetpackPlugin } from "./scripts/assetpack-vite-plugin";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [wasm(), assetpackPlugin()],
+  plugins: [wasm(), assetpackPlugin(), cloudflare()],
   server: {
     port: 8080,
     open: true,
